1a72316caa75e5ab9db600abb7cad0b97a622a77
harishtalluru <harshatalluru20@gmail.com>
2022-07-10 14:09:43 +0530

    first commit

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './profile.css'
-//import Typical from 'react-typical'
+import Typical from 'react-typical'
 import award from './harish.png'
 export default function Profile() {
 
@@ -14,13 +14,13 @@ export default function Profile() {
           <h3 className="i-intro" style={{fontWeight:"100px",color:"darksalmon"}}>Hello, My name is</h3>
           <h1 className="i-name" style={{color:"pilepink"}}>Harish Talluru</h1>
           <div className="i-title">
-         <h2 style={{color:"orange"}}>Full Stack Developer
-           {/* <Typical
+         <h2 style={{color:"orange"}}>
+            <Typical
 
 steps={['  Full stack developer', 3000, 'Front-End Developer', 2000,'Back-End Developer',2000]}
 loop={Infinity}
 
-/>*/}
+/>
           </h2>
           </div>
           <p className="i-desc">
